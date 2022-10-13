5b8b239b4bbeb484b27533f849f0bd7869b04358
sigwi <107711109+sigwi@users.noreply.github.com>
2022-10-14 00:33:50 +0700

    Update read_local_json.dart

--- read_local_json.dart
+++ read_local_json.dart
@@ -1,6 +1,16 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-  List<dynamic>? dataDecode; //decode data from json file
-  List<dynamic>? dataReady; //data to deploy on the screen
+class CountryList extends StatefulWidget {
+  const CountryList({super.key});
+
+  @override
+  State<CountryList> createState() => _CountryListState();
+}
+
+class _CountryListState extends State<CountryList> {
+  late List<dynamic> dataDecode; //decode data from json file
 
   @override
   void initState() {
@@ -8,14 +18,56 @@
     _getData();
   }
 
-  List<dynamic> docIDs = [];
+  List dataReady = [];
 
-  _getData() async {
+  Future<void> _getData() async {
     final String response =
         await rootBundle.loadString('assets/CountryCodes.json');
-    dataDecode = await json.decode(response) as List<dynamic>;
+    dataDecode = await json.decode(response);
 
     setState(() {
       dataReady = dataDecode;
     });
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple[500],
+        elevation: 5,
+        title: Text('Select Country'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          children: [
+            //Display the data loaded from sample.json
+            dataReady.isNotEmpty
+                ? Expanded(
+                    child: ListView.builder(
+                      itemCount: dataReady.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          key: ValueKey(dataReady[index]["name"]),
+                          margin: const EdgeInsets.all(3),
+                          color: Colors.amber.shade100,
+                          child: ListTile(
+                            leading: Text(dataReady[index]["name"]),
+                            // subtitle: Text(dataReady[index]["code"]),
+                            // title: Text(dataReady[index]["name"]),
+                            trailing: Text(dataReady[index]["dial_code"]),
+                          ),
+                        );
+                      },
+                    ),
+                  )
+                : Center(
+                    child: Text('Loading...'),
+                  )
+          ],
+        ),
+      ),
+    );
+  }
+}
